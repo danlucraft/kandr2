44c95997ec13efa2dddb4cf9b2f5572c52bdae93
Dan Lucraft <dan@songkick.com>
2016-06-03 12:26:32 +0100

    Make rightrot function actually *rotate* rather than rightshift

--- ex_2_08.c
+++ ex_2_08.c
@@ -5,19 +5,12 @@
 Write a function rightrot(x, n) that returns the value of the integer x 
 rotated to the right by n bit positions.
 
+NB. I thought this was just a re-implementation of >>, but "rotate" means
+the bits at the end go to the front.
 */
 
 #include <stdio.h>
-
-
-unsigned int rightrot(unsigned int x, int n);
-unsigned int rightrot(unsigned int x, int n)
-{
-    while(n-- > 0)
-        x /= 2;
-    return x;
-}
-
+#include <limits.h>
 
 void print_bits(unsigned int x);
 void print_bits(unsigned int x)
@@ -33,6 +26,18 @@ void print_bits(unsigned int x)
 	printf("%s", s);
 }
 
+unsigned int rightrot(unsigned int x, int n);
+unsigned int rightrot(unsigned int x, int n)
+{
+    unsigned int right_mask    = ~((unsigned) ~0 << n);
+    unsigned int left_mask     = ~right_mask;
+    unsigned int right         = x & right_mask;
+    unsigned int left          = x & left_mask;
+    unsigned int right_rotated = right << ((int) (sizeof x)*8 - n);
+    unsigned int left_rotated  = left >> n;
+    return right_rotated | left_rotated;
+}
+
 void test_rightrot(unsigned int x, int n);
 void test_rightrot(unsigned int x, int n)
 {
@@ -43,13 +48,20 @@ void test_rightrot(unsigned int x, int n)
     printf("\n");
 }
 
-
 int main()
 {
-    test_rightrot(245, 1);
-    test_rightrot(245, 2);
-    test_rightrot(245, 3);
-    test_rightrot(245, 4);
-    test_rightrot(245, 5);
-    test_rightrot(245, 6);
+    unsigned int p = 5;
+    printf("sizeof(unsigned int) in bytes = %lu\n", sizeof p);
+    printf("sizeof(unsigned int) in bits = %lu\n", (sizeof p)*8);
+    test_rightrot(245-64 + 65536, 4);
+    test_rightrot(UINT_MAX, 4);
 }
+/*
+
+$ clang -g -Wno-newline-eof -Weverything ex_2_08.c && ./a.out
+sizeof(unsigned int) in bytes = 4
+sizeof(unsigned int) in bits = 32
+rightrot(00000000000000010000000010110101, 4) = 01010000000000000001000000001011
+rightrot(11111111111111111111111111111111, 4) = 11111111111111111111111111111111
+
+*/
