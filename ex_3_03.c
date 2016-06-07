d3bb0c5452cb692f497c4441d220178bd39328de
Dan Lucraft <dan@songkick.com>
2016-06-07 14:26:56 +0100

    Tweaks

--- ex_3_03.c
+++ ex_3_03.c
@@ -20,24 +20,21 @@ int lowercase_pair(int c1, int c2);
 int lowercase_pair(int c1, int c2)
 {
 	return (c1 >= 'a' && c1 <= 'z') &&
-		(c2 >= 'a' && c2 <= 'z') && 
-		c1 < c2;
+		(c2 >= 'a' && c2 <= 'z');
 }
 
 int uppercase_pair(int c1, int c2);
 int uppercase_pair(int c1, int c2)
 {
 	return (c1 >= 'A' && c1 <= 'Z') &&
-		(c2 >= 'A' && c2 <= 'Z') &&
-		c1 < c2;
+		(c2 >= 'A' && c2 <= 'Z');
 }
 
 int digit_pair(int c1, int c2);
 int digit_pair(int c1, int c2)
 {
 	return (c1 >= '0' && c1 <= '9') &&
-		(c2 >= '0' && c2 <= '9') &&
-		c1 < c2;
+		(c2 >= '0' && c2 <= '9');
 }
 
 void expand(char* s1, char* s2);
@@ -48,16 +45,15 @@ void expand(char* s1, char* s2)
 
 	while (s1[i] != '\0') {
 		int start = s1[i];
-		s2[j] = s1[i];
-		i++;
-		j++;
+		s2[j++] = s1[i++];
 
 		if (s1[i] == '-') {
 			if (s1[i + 1] != '\0') {
 				int end = s1[i + 1];
-				if (lowercase_pair(start, end) ||
-						uppercase_pair(start, end) ||
-						digit_pair(start, end)) {
+				if (start < end && 
+						(lowercase_pair(start, end) ||
+						 uppercase_pair(start, end) ||
+						 digit_pair(start, end))) {
 					for (int c = start + 1; c <= end; c++) {
 						s2[j] = (char) c;
 						j++;
@@ -97,4 +93,18 @@ int main()
 
 $ clang -Weverything ex_3_03.c && ./a.out
 
+test 1:
+a-e    2-9
+abcde    23456789
+abcde    23456789
+
+test 2:
+-f-l123a-b-c a-e-z x-z0-9
+-fghijkl123ab-c abcde-z xyz0123456789
+-fghijkl123ab-c abcde-z xyz0123456789
+
+test 3:
+9-3  e-a
+9-3  e-a
+9-3  e-a
 */
