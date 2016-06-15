d575ff5ea9d4b252b9746d04b155bfc372a3ae9e
Dan Lucraft <dan@songkick.com>
2016-06-15 13:54:23 +0100

    Ex 4.05: rewrite to support multicharacter commands

--- chapter4/ex_4_05.c
+++ chapter4/ex_4_05.c
@@ -11,11 +11,13 @@ Add access to library functions like sin, exp, and pow
 
 #define MAXOP   100 /* max size of operand or operator */
 #define NUMBER  '0' /* signal that a number was found */
+#define OP      '1' /* signal that it's an operation / command */
 
 int getop(char []);
 void push(double);
 double pop(void);
 void clear(void);
+int streq(char* a, char* b);
 
 /* reverse polish calculator */
 int main()
@@ -25,56 +27,47 @@ int main()
 	char s[MAXOP];
 
 	while ((type = getop(s)) != EOF) {
-		switch (type) {
-		case NUMBER:
+		if (type == NUMBER) {
 			push(atof(s));
-			break;
-		case '+':
-			push(pop() + pop());
-			break;
-		case '*':
-			push(pop() * pop());
-			break;
-		case '-':
-			op2 = pop();
-			push(pop() - op2);;
-			break;
-		case '/':
-			op2 = pop();
-			if (op2 != 0.0)
-				push(pop() / op2);
-			else
-				printf("error: zero divisor\n");
-			break;
-		case '%':
-			op2 = pop();
-			push(fmod(pop(),op2));
-			break;
-		case 'p':
-			op2 = pop();
-			printf("\t%.8g\n", op2);
-			push(op2);
-			break;
-		case 'd':
-			op2 = pop();
-			push(op2);
-			push(op2);
-			break;
-		case 's':
-			op1 = pop();
-			op2 = pop();
-			push(op1);
-			push(op2);
-			break;
-		case 'c':
-			clear();
-			break;
-		case '\n':
-			printf("\t%.8g\n", pop());
-			break;
-		default:
-			printf("error: unknown command %s\n", s);
-			break;
+		} else if (type == OP) {
+			if (streq("+", s)) {
+				push(pop() + pop());
+			} else if (streq("*", s)) {
+				push(pop() * pop());
+			} else if (streq("-", s)) {
+				op2 = pop();
+				push(pop() - op2);;
+			} else if (streq("/", s)) {
+				op2 = pop();
+				if (op2 != 0.0)
+					push(pop() / op2);
+				else
+					printf("error: zero divisor\n");
+			} else if (streq("%", s)) {
+				op2 = pop();
+				push(fmod(pop(),op2));
+			} else if (streq("p", s) || streq("peek", s)) {
+				op2 = pop();
+				printf("\t%.8g\n", op2);
+				push(op2);
+			} else if (streq("d", s) || streq("dup", s)) {
+				op2 = pop();
+				push(op2);
+				push(op2);
+			} else if (streq("s", s) || streq("swap", s)) {
+				op1 = pop();
+				op2 = pop();
+				push(op1);
+				push(op2);
+			} else if (streq("c", s) || streq("clear", s)) {
+				clear();
+			} else if (streq("\n", s)) {
+				printf("\t%.8g\n", pop());
+			} else {
+				printf("error: unknown command %s\n", s);
+			}
+		} else {
+			printf("unknown token type: '%c'\n", type);
 		}
 	}
 	return 0;
@@ -112,40 +105,67 @@ void clear(void)
 	sp = 0;
 }
 
+int streq(char* a, char* b)
+{
+	int i = 0;
+	while (a[i] != '\0') {
+		if (a[i] != b[i])
+			return 0;
+		i++;
+	}
+	if (b[i] != '\0')
+		return 0;
+	return 1;
+}
+
 static int getch(void);
 static void ungetch(int);
 
 /* getop:   get next operator or numeric operand */
 int getop(char s[])
 {
-	int i, c;
+	int i = 0;
+	int c;
 
 	while ((c = getch(), s[0] = (char) c) == ' ' || c == '\t')
 		;
+
 	s[1] = '\0';
-	if (!isdigit(c) && c != '.' && c != '-')
-		return c;    /* not a number */
+	i = 1;
 
-	if (c == '-') {
-		/* get then ungetch the next character to see if this
-		 * is a negative number or not */
-		c = getch();
-		ungetch(c);
-		if (!isdigit(c) && c != '.') {
-			return '-';
-		}
-	}
+	if (streq(s, "\n"))
+		return OP;
 
-	i = 0;
-	if (isdigit(c))  /* collect integer part */
-		while (c = getch(), s[++i] = (char) c, isdigit(s[i]))
-			;
-	if (c == '.')    /* collect fraction part */
-		while (c = getch(), s[++i] = (char) c, isdigit(s[i]))
-			;
-	s[i] = '\0';
-	if (c != EOF)
-		ungetch(c);
+	while ((c = getch(), s[i++] = (char) c) != ' ' && c != '\t' && c != '\n' && c != EOF)
+		;
+	ungetch(c);
+
+	s[i - 1] = '\0';
+
+	// the rest of this function checks if the string in s is a number
+	int j = 0;
+
+	if (s[j] == '-')
+		j++;
+
+	if (!isdigit(s[j]) && s[j] != '.')
+		return OP;
+
+	while (isdigit(s[j]))
+		j++;
+
+	if (s[j] != '.' && s[j] != '\0')
+		return OP;
+
+	if (s[j] == '.')
+		j++;
+
+	while (isdigit(s[j]))
+		j++;
+
+	if (s[j] != '\0')
+		return OP;
+
 	return NUMBER;
 }
 
@@ -169,20 +189,14 @@ void ungetch(int c) /* push character back on input */
 
 /*
 
-$ clang -g -Weverything chapter4/ex_4_04.c && ./a.out
-1 p 2 p +
-    1
-	2
+$ clang -Weverything chapter4/ex_4_05.c && ./a.out
+1 2 +
 	3
 1 d +
 	2
 2 1 -
 	1
-2 1 s -
+2 1 swap -
 	-1
-1 2 c +
-error: stack empty
-error: stack empty
-    0
 
 */
