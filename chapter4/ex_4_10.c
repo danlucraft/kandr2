2cb1729b766cf27c9948066e046d512448b1390e
Dan Lucraft <dan@songkick.com>
2016-06-22 13:36:56 +0100

    Ex 4.10: replace getch and ungetch with get_line

--- chapter4/ex_4_10.c
+++ chapter4/ex_4_10.c
@@ -1,4 +1,8 @@
 /*
+
+An alternate organization uses getline to read an entire input line; this makes 
+getch and ungetch unnecessary. Revise the calculator to use this approach.
+
 */
 
 #include <stdio.h>
@@ -9,13 +13,16 @@
 #define MAXOP   100 /* max size of operand or operator */
 #define NUMBER  '0' /* signal that a number was found */
 #define OP      '1' /* signal that it's an operation / command */
+#define MAXLINE 100 /* max length of input line */
 
-void debug_buf(void);
-int getop(char []);
+int get_line(char s[]);
+int getop(char[], int, char[]);
 void push(double);
 double pop(void);
 void clear(void);
 int streq(char* a, char* b);
+int str_len(char[]);
+int type_of_op(char[]);
 
 static double vars[26];
 static double last;
@@ -26,77 +33,84 @@ int main()
 	int type;
 	double op1, op2;
 	char s[MAXOP];
+	char line[MAXLINE];
+	int line_len;
 
-	while ((type = getop(s)) != EOF) {
-		if (type == NUMBER) {
-			push(atof(s));
-		} else if (type == OP) {
-			// arithmetic
-			if (streq("+", s)) {
-				push(pop() + pop());
-			} else if (streq("*", s)) {
-				push(pop() * pop());
-			} else if (streq("-", s)) {
-				op2 = pop();
-				push(pop() - op2);;
-			} else if (streq("/", s)) {
-				op2 = pop();
-				if (op2 != 0.0)
-					push(pop() / op2);
-				else
-					printf("error: zero divisor\n");
-			} else if (streq("%", s)) {
-				op2 = pop();
-				push(fmod(pop(),op2));
+	while ((line_len = get_line(line)) != EOF) {
+		int from = 0;
+		while (from < line_len) {
+			from = getop(line, from, s);
+			type = type_of_op(s);
+			if (type == NUMBER) {
+				push(atof(s));
+			} else if (type == OP) {
+				// arithmetic
+				if (streq("+", s)) {
+					push(pop() + pop());
+				} else if (streq("*", s)) {
+					push(pop() * pop());
+				} else if (streq("-", s)) {
+					op2 = pop();
+					push(pop() - op2);;
+				} else if (streq("/", s)) {
+					op2 = pop();
+					if (op2 != 0.0)
+						push(pop() / op2);
+					else
+						printf("error: zero divisor\n");
+				} else if (streq("%", s)) {
+					op2 = pop();
+					push(fmod(pop(),op2));
 
-			// stack manipulation 
-			} else if (streq("p", s) || streq("peek", s)) {
-				op2 = pop();
-				printf("\t%.8g\n", op2);
-				push(op2);
-			} else if (streq("d", s) || streq("dup", s)) {
-				op2 = pop();
-				push(op2);
-				push(op2);
-			} else if (streq("s", s) || streq("swap", s)) {
-				op1 = pop();
-				op2 = pop();
-				push(op1);
-				push(op2);
-			} else if (streq("c", s) || streq("clear", s)) {
-				clear();
+				// stack manipulation 
+				} else if (streq("p", s) || streq("peek", s)) {
+					op2 = pop();
+					printf("\t%.8g\n", op2);
+					push(op2);
+				} else if (streq("d", s) || streq("dup", s)) {
+					op2 = pop();
+					push(op2);
+					push(op2);
+				} else if (streq("s", s) || streq("swap", s)) {
+					op1 = pop();
+					op2 = pop();
+					push(op1);
+					push(op2);
+				} else if (streq("c", s) || streq("clear", s)) {
+					clear();
 
-			// maths
-			} else if (streq("sin", s)) {
-				push(sin(pop()));
-			} else if (streq("cos", s)) {
-				push(cos(pop()));
-			} else if (streq("tan", s)) {
-				push(tan(pop()));
-			} else if (streq("exp", s)) {
-				push(exp(pop()));
-			} else if (streq("pow", s)) {
-				op2 = pop();
-				op1 = pop();
-				push(pow(op1, op2));
+				// maths
+				} else if (streq("sin", s)) {
+					push(sin(pop()));
+				} else if (streq("cos", s)) {
+					push(cos(pop()));
+				} else if (streq("tan", s)) {
+					push(tan(pop()));
+				} else if (streq("exp", s)) {
+					push(exp(pop()));
+				} else if (streq("pow", s)) {
+					op2 = pop();
+					op1 = pop();
+					push(pow(op1, op2));
 
-			// pop and print
-			} else if (streq("\n", s)) {
-				last = pop();
-				printf("\t%.8g\n", last);
+				// pop and print
+				} else if (streq("\n", s)) {
+					last = pop();
+					printf("\t%.8g\n", last);
 
-			// variables
-			} else if (s[0] == '>') {
-				vars[s[1] - 'a'] = pop();
-			} else if (s[1] == '>') {
-				push(vars[s[0] - 'a']);
-			} else if (streq("_", s)) {
-				push(last);
+				// variables
+				} else if (s[0] == '>') {
+					vars[s[1] - 'a'] = pop();
+				} else if (s[1] == '>') {
+					push(vars[s[0] - 'a']);
+				} else if (streq("_", s)) {
+					push(last);
+				} else {
+					printf("error: unknown command %s\n", s);
+				}
 			} else {
-				printf("error: unknown command %s\n", s);
+				printf("unknown token type: '%c'\n", type);
 			}
-		} else {
-			printf("unknown token type: '%c'\n", type);
 		}
 	}
 	return 0;
@@ -147,16 +161,14 @@ int streq(char* a, char* b)
 	return 1;
 }
 
-static int getch(void);
-static void ungetch(int);
 
 /* getop:   get next operator or numeric operand */
-int getop(char s[])
+int getop(char line[], int from, char s[])
 {
 	int i = 0;
 	int c;
 
-	while ((c = getch(), s[0] = (char) c) == ' ' || c == '\t')
+	while ((c = line[from++], s[0] = (char) c) == ' ' || c == '\t')
 		;
 
 	s[1] = '\0';
@@ -167,12 +179,17 @@ int getop(char s[])
 	if (s[0] == EOF)
 		return EOF;
 
-	while ((c = getch(), s[i++] = (char) c) != ' ' && c != '\t' && c != '\n' && c != EOF)
+	while ((c = line[from++], s[i++] = (char) c) != ' ' && c != '\t' && c != '\n' && c != EOF)
 		;
-	ungetch(c);
+	from--;
 
 	s[i - 1] = '\0';
 
+	return from;
+}
+
+int type_of_op(char s[])
+{
 	// the rest of this function checks if the string in s is a number
 	int j = 0;
 
@@ -200,29 +217,35 @@ int getop(char s[])
 	return NUMBER;
 }
 
-#define BUFSIZE 100
-
-static char buf[BUFSIZE]; /* buffer for ungetch */
-static int bufp = 0;      /* next free position in buf */
-
-void debug_buf()
+// returns EOF if reached the end,
+// otherwise the length of the line returned
+// including \n
+// does NOT include line terminator
+int get_line(char s[])
 {
-	printf("bufp: %d, [", bufp);
-	for (int i = 0; i < bufp; i++)
-		printf("%d, ", buf[i]);
-	printf("]\n");
+	int i = 0;
+	int c = 0;
+	while (i < MAXLINE && (c = getchar()) && c != '\n' && c != EOF)
+		s[i++] = (char) c;
+	if (c == EOF)
+		return EOF;
+	if (c == '\n' && i < MAXLINE) { // otherwise would try to put two \n in a row at the end of the line
+		s[i++] = (char) c;
+		return i + 1;
+	}
+	return i;
 }
 
-int getch(void) /* get a (possibly pushed back) character */
-{
-	return (bufp > 0) ? buf[--bufp] : getchar();
-}
+/*
 
-void ungetch(int c) /* push character back on input */
-{
-	if (bufp >= BUFSIZE)
-		printf("ungetch: too many characters\n");
-	else
-		buf[bufp++] = (char) c;
-}
+$ clang -Weverything chapter4/ex_4_10.c && ./a.out
+1 2 +
+		3
+1 sin 2 +
+		2.841471
+10 >a 3
+		3
+6 a> *
+		60
 
+*/
