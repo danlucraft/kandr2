9a7be377949144e9e32a014e7a1caffa0a29a619
Dan Lucraft <dan@songkick.com>
2016-06-28 10:12:23 +0100

    Ex 4.12: implement recursive itoa

--- chapter4/ex_4_12.c
+++ chapter4/ex_4_12.c
@@ -1,5 +1,10 @@
 /*
 
+Write a recursive version of itoa; that is, convert an integer into a string 
+by calling a recursive routine
+
+danlucraft: I found this one a little mind-bending, and I'm not sure if
+the code is particularly easy to understand, though it is clean.
 
 */
 
@@ -7,32 +12,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-void reverse(char s[]);
-void reverse(char s[])
+int itoa1(int n, char s[], int start);
+int itoa1(int n, char s[], int start)
 {
-	int c, i, j;
-
-	for (i = 0, j = (int) strlen(s)-1; i < j; i++, j--) {
-		c = s[i];
-		s[i] = s[j];
-		s[j] = (char) c;
-	}
+	char ch = (char) abs(n % 10) + '0';
+	int i = (n / 10) ? itoa1(n / 10, s, start) : start;
+	s[i] = ch;
+	return i + 1;
 }
 
 void itoa(int n, char s[]);
 void itoa(int n, char s[])
 {
-	int i, sign;
-
-	sign = n;
-	i = 0;
-	do {
-		s[i++] = (char) abs(n % 10) + '0';
-	} while (abs(n /= 10) > 0);
-	if (sign < 0)
-		s[i++] = '-';
-	s[i] = '\0';
-	reverse(s);
+	int start = 0;
+	if (n < 0) {
+		s[0] = '-';
+		start = 1;
+	}
+	itoa1(n, s, start);
 }
 
 void testitoa(int);
@@ -43,12 +40,22 @@ void testitoa(int n)
 	printf("itoa(%d) = %s\n", n, s);
 }
 
-
 int main()
 {
 	testitoa(123);
 	testitoa(56789);
+	testitoa(-22);
 	testitoa(1);
 	testitoa(0);
 }
 
+/*
+
+$ clang chapter4/ex_4_12.c && ./a.out
+itoa(123) = 123
+itoa(56789) = 56789
+itoa(-22) = -22
+itoa(1) = 1
+itoa(0) = 0
+
+*/
