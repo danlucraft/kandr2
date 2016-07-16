99a70eec24bb3128d1e1d77e2163e5ada0adf326
Daniel Lucraft <dan@lucraft.me>
2016-07-16 10:16:21 +0100

    Ex 5.11.detab: commmit previous detab code

---
+++ chapter5/ex_5_11_detab.c
@@ -0,0 +1,43 @@
+/* 
+
+Modify the program detab to accept a list of tab stops as arguments. Use the
+default tab settings if there are no arguments.
+
+//        1         2         3         4         5         6         7         8         9
+//		1		2		3		4		5		6		7		8		9		10		11		12
+
+to inspect result:
+clang -g -fsanitize=address -Wno-newline-eof -Weverything ex_1_20_detab.c && cat ex_1_20_detab.c | ./a.out | ruby -e 'STDIN.read.split("\n").each {|l| p l}'
+
+*/
+
+#include <stdio.h>
+
+#define TAB_WIDTH  4
+
+int main()
+{
+	int column = 0;
+	int tab_width = TAB_WIDTH;
+	int c;
+	while ((c = getchar()) && c != EOF) {
+		if (c == '\t') {
+			int new_column = ((column + tab_width) / tab_width) * tab_width;
+			for (int spaces = new_column - column; spaces > 0; spaces--)
+				putchar(' ');
+			column = new_column;
+		}
+		else if (c == '\n') {
+			column = 0;
+			putchar(c);
+		}
+		else {
+			column++;
+			putchar(c);
+		}
+	}
+}
+
+// test line:
+//	abc
+//	a	b
