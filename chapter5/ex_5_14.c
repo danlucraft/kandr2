7b4aff1c5b7baf56af5ada6e6a3c14caec3df14b
Daniel Lucraft <dan@lucraft.me>
2016-07-18 21:27:28 +0100

    Ex 5.14: implement -r flag for reverse sortt

--- chapter5/ex_5_14.c
+++ chapter5/ex_5_14.c
@@ -1,7 +1,7 @@
 /*
 
-Example from section 5.11, modifying the sort programme to run a numeric sort 
-if given -n option.
+Modify the sort program to handl a -r flag, which indicates sorting in reverse
+(decreasing) order.
 
 */
 
@@ -17,24 +17,30 @@ int readlines(char *linep[], char allocbuf[], int nlines);
 void writelines(char *linep[], int nlines);
 
 void my_qsort(void *linep[], int left, int right,
-		   int (*comp)(void *, void *));
+			   int (*comp)(void *, void *),
+			   int comp_factor);
 int numcmp(const char *, const char *);
 void swap(void *v[], int i, int j);
 
 static char allocbuf[ALLOCSIZE];
 static char *lineptr[MAXLINES];
 
-int main(int argc, char *argv[])
+int main(int __unused argc, char *argv[])
 {
 	int nlines;
 	int numeric = 0;
+	int comp_factor = 1;
 
-	if (argc > 1 && strcmp(argv[1], "-n") == 0)
-		numeric = 1;
+	while (*++argv) {
+		if (strcmp(argv[0], "-n") == 0)
+			numeric = 1;
+		if (strcmp(argv[0], "-r") == 0)
+			comp_factor = -1;
+	}
 
 	if ((nlines = readlines(lineptr, allocbuf, MAXLINES)) >= 0) {
 		my_qsort((void **) lineptr, 0, nlines - 1,
-			  (int (*)(void*,void*))(numeric ? numcmp : strcmp));
+			  (int (*)(void*,void*))(numeric ? numcmp : strcmp), comp_factor);
 		writelines(lineptr, nlines);
 		return 0;
 	} else {
@@ -84,7 +90,8 @@ int get_line(char *s, int lim)
 }
 
 void my_qsort(void *v[], int left, int right,
-		      int (*comp)(void *, void *))
+		      int (*comp)(void *, void *),
+			  int comp_factor)
 {
 	int i, last;
 	if (left >= right)
@@ -93,11 +100,11 @@ void my_qsort(void *v[], int left, int right,
 	swap(v, left, (left+right)/2);
 	last = left;
 	for (i = left+1; i <= right; i++)
-		if ((*comp)(v[i], v[left]) < 0)
+		if ((*comp)(v[i], v[left]) * comp_factor < 0)
 			swap(v, ++last, i);
 	swap(v, left, last);
-	my_qsort(v, left, last-1, comp);
-	my_qsort(v, last+1, right, comp);
+	my_qsort(v, left, last-1, comp, comp_factor);
+	my_qsort(v, last+1, right, comp, comp_factor);
 }
 
 void swap(void *v[], int i, int j)
@@ -124,32 +131,18 @@ int numcmp(const char *s1, const char *s2)
 
 /*
 
-$ clang -Weverything chapter5/ex_5_14.c && cat chapter5/ex_5_14.test | ./a.out
-1
-11
-123
+$ clang -Weverything chapter5/ex_5_14.c && cat chapter5/ex_5_14.test | ./a.out -r -n
 222222
-23
-34
-35
-56
-6
-64
-67
-89
-~/Dropbox/Code/kandr2 (master)
-$ clang -Weverything chapter5/ex_5_14.c && cat chapter5/ex_5_14.test | ./a.out -n
-1
-6
-11
-23
-34
-35
-56
-64
-67
-89
 123
-222222
+89
+67
+64
+56
+35
+34
+23
+11
+6
+1
 
 */
