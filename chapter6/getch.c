7c8061c8fc8f531662770fa481bd9cda457c12ae
Daniel Lucraft <dan@lucraft.me>
2016-07-25 12:09:45 +0100

    Ex 6.01: commit sample code from section 6.3

---
+++ chapter6/getch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "getch.h"
+
+#define BUFSIZE 100
+
+static char buf[BUFSIZE]; /* buffer for ungetch */
+static int bufp = 0;      /* next free position in buf */
+
+int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = (char) c;
+}
+
