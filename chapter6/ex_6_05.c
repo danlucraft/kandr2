43b7f4d01d55b089e07b382486f04b7373ea33c7
Daniel Lucraft <dan@lucraft.me>
2016-07-28 13:28:36 +0100

    Ex 6.05: write a function to remove a key & value from the table

--- chapter6/ex_6_05.c
+++ chapter6/ex_6_05.c
@@ -1,6 +1,7 @@
 /*
 
-Example code from Section 6.6, a simple hashtable
+Write a function undef that will remove a name and definition from the table
+maintained by lookup and install.
 
 */
 
@@ -14,7 +15,7 @@ struct nlist {
 	char *defn;
 };
 
-#define HASHSIZE 101
+#define HASHSIZE 2
 
 static struct nlist *hashtab[HASHSIZE];
 
@@ -47,7 +48,12 @@ struct nlist *install(char *name, char *defn)
 
 	if ((np = lookup(name)) == NULL) {
 		np = malloc(sizeof *np);
-		if (np == NULL || (np->name = strdup(name)) == NULL)
+		if (np == NULL)
+			return NULL;
+		np->next = NULL;
+		np->name = NULL;
+		np->defn = NULL;
+		if ((np->name = strdup(name)) == NULL)
 			return NULL;
 		hashval = hash(name);
 		np->next = hashtab[hashval];
@@ -58,6 +64,34 @@ struct nlist *install(char *name, char *defn)
 	if ((np->defn = strdup(defn)) == NULL)
 		return NULL;
 	return np;
+}
+
+void undef(char *name);
+void undef(char *name)
+{
+	struct nlist *found = NULL, *np = NULL, *prev = NULL;
+
+	unsigned hashval = hash(name);
+
+	if (hashtab[hashval] == NULL)
+		return;
+
+	if (strcmp(name, hashtab[hashval]->name) == 0) {
+		found = hashtab[hashval];
+		hashtab[hashval] = hashtab[hashval]->next;
+	} else {
+		for (prev = hashtab[hash(name)], np = prev->next; np != NULL; prev = np, np = np->next)
+			if (strcmp(name, np->name) == 0) {
+				found = np;
+				prev->next = np->next;
+			}
+	}
+
+	if (found != NULL) {
+		free((void *) found->name);
+		free((void *) found->defn);
+		free((void *) found);
+	}
 }
 
 void test_lookup(char *name);
@@ -71,11 +105,47 @@ int main()
 {
 	install("hello", "world");
 	install("hi", "there");
+
+	install("hello1", "world1");
+	install("hi1", "there1");
+	install("hello2", "world2");
+	install("hi2", "there2");
+	install("hello3", "world3");
+	install("hi3", "there3");
+	install("hello4", "world4");
+	install("hi4", "there4");
+	install("hello5", "world5");
+	install("hi5", "there5");
+	install("hello6", "world6");
+	install("hi6", "there6");
+
 	test_lookup("hi");
+
+	// exercises the code that finds it at the start of the list
+	test_lookup("hello4");
+	printf("undefining \"hello4\"\n");
+	undef("hello4");
+	test_lookup("hello4");
+
+	// exercises the code that finds it in the middle / end of the list
 	test_lookup("hello");
+	printf("undefining \"hello\"\n");
+	undef("hello");
+	test_lookup("hello");
+
 	test_lookup("nuhuh");
 }
 
+/*
 
+$ clang -Weverything -Wno-padded chapter6/ex_6_05.c && ./a.out
+hi -> there
+hello6 -> world6
+undefining "hello6"
+hello6 -> <not found>
+hello -> world
+undefining "hello"
+hello -> <not found>
+nuhuh -> <not found>
 
-
+*/
