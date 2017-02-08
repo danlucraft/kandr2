b67d1f732d09c4e9d74f29da5af8be7aa418a41f
Dan Lucraft <dan@charliehr.com>
2017-02-08 08:56:41 +0000

    Ex 1.09: tweak output

--- chapter1/ex_1_09.c
+++ chapter1/ex_1_09.c
@@ -1,3 +1,6 @@
+/* Write a program to copy its input to its output, replacing each string of
+ * one or more blanks by a single blank. */
+
 #include   <stdio.h>
 
 int main()
@@ -5,6 +8,8 @@ int main()
 	int  c;
 
 	int prevspace = 0;
+
+	// NB next line
 	while ((c =        getchar()) != EOF) {
 		if (c != ' ' || !prevspace) {
 			putchar(c);
@@ -15,20 +20,19 @@ int main()
 	}
 }
 
-// $ clang 1_9.c && cat 1_9.c | ./a.out
-// #include <stdio.h>
+// $ clang chapter1/ex_1_09.c && head -n 15 chapter1/ex_1_09.c | ./a.out
+///* Write a program to copy its input to its output, replacing each string of
+// * one or more blanks by a single blank. */
 //
-// int main()
-// {
-// 	int c;
+//#include <stdio.h>
 //
-// 	int prevspace = 0;
-// 	while ((c = getchar()) != EOF) {
-// 		if (c != ' ' || !prevspace) {
-// 			putchar(c);
-// 			prevspace = 0;
-// 		}
-// 		if (c == ' ')
-// 			prevspace = 1;
-// 	}
-// }
+//int main()
+//{
+//	int c;
+//
+//	int prevspace = 0;
+//
+//	// NB next line
+//	while ((c = getchar()) != EOF) {
+//		if (c != ' ' || !prevspace) {
+//			putchar(c);
