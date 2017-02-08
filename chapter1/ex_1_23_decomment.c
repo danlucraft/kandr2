061b95d595705b26c331b3fe94ea76807a46cf7f
Dan Lucraft <dan@charliehr.com>
2017-02-08 20:56:19 +0000

    Ex 1.23: include output

--- chapter1/ex_1_23_decomment.c
+++ chapter1/ex_1_23_decomment.c
@@ -1,7 +1,6 @@
 /* Removes comments from a C program (both styles) */
 
 // inspect output with
-// clang -Wno-newline-eof -Weverything ex_1_23_decomment.c && cat ex_1_23_decomment.c | ./a.out
 #include <stdio.h>
 
 int main()// yup
@@ -65,3 +64,70 @@ int main()// yup
 		prevc = c;
 	}
 }
+
+// $ clang -Wno-int-conversion -Wno-comment -Wno-multichar -Wno-unused-variable -Wno-newline-eof -Weverything chapter1/ex_1_23_decomment.c && head -n 66 chapter1/ex_1_23_decomment.c | ./a.out
+// 
+// 
+// 
+// #include <stdio.h>
+// 
+// int main()
+// {
+// 	char test[] = "hi /* boo";
+// 	char test2[] = "hi // bar";
+// 	char test3 = (char) '/*';
+// 	char test4 = (char) '//';
+// 	char test5 = "hi \" /* ";
+// 	char test6 = "hi ' /* ";
+// 
+// 
+// 
+// 	int in_c_comment = 0;
+// 	int in_cpp_comment = 0;
+// 	int in_string = 0;
+// 	int in_character_literal = 0;
+// 
+// 	int c;
+// 	int prevc = 0;
+// 	while ((c = getchar()) != EOF) {
+// 		if (!in_c_comment && !in_cpp_comment &&
+// 			!in_string && !in_character_literal) {
+// 			if (prevc == '/' && c == '*')
+// 				in_c_comment = 1;
+// 			if (c == '/' && prevc == '/')
+// 				in_cpp_comment = 1;
+// 		}
+// 
+// 		if (c == '"') {
+// 			if (in_string) {
+// 				if (prevc != '\\')
+// 					in_string = 0;
+// 			} else if (!in_c_comment && !in_cpp_comment && !in_character_literal) {
+// 				in_string = 1;
+// 			}
+// 		}
+// 
+// 
+// 		if (c == '\'') {
+// 			if (in_character_literal) {
+// 				if (prevc != '\\')
+// 					in_character_literal = 0;
+// 			} else if (!in_c_comment && !in_cpp_comment && !in_string) {
+// 				in_character_literal = 1;
+// 			}
+// 		}
+// 
+// 		if (!in_c_comment && !in_cpp_comment && prevc)
+// 			putchar(prevc);
+// 
+// 		if (in_c_comment)
+// 			if (prevc == '*' && c == '/') {
+// 				in_c_comment = 0;
+// 				c = 0;
+// 			}
+// 		if (in_cpp_comment)
+// 			if (c == '\n')
+// 				in_cpp_comment = 0;
+// 
+// 		prevc = c;
+// 	}
